8e7995aa759d611863efa05ebca6178380d4450d
Leo Xu <xulianghhu@gmail.com>
2015-02-13 12:05:43 +0800

    add provinc and city selection

--- src/com/example/zf_android/trade/entity/Province.java
+++ src/com/example/zf_android/trade/entity/Province.java
@@ -1,11 +1,12 @@
 package com.example.zf_android.trade.entity;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by Leo on 2015/2/11.
  */
-public class Province {
+public class Province implements Serializable {
     private int id;
     private String name;
     private List<City> cities;
@@ -32,5 +33,18 @@ public class Province {
 
     public void setCities(List<City> cities) {
         this.cities = cities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (null == o) return false;
+        if (!(o instanceof Province)) return false;
+        Province that = (Province) o;
+        return that.getId() == this.getId();
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
