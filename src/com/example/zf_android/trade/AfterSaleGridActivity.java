7f6a7e496bf3c97a9f2c284265ab99ce84ea7e9e
Leo <xulianghhu@gmail.com>
2015-02-26 11:49:06 +0800

    add after sale module

---
+++ src/com/example/zf_android/trade/AfterSaleGridActivity.java
@@ -0,0 +1,18 @@
+package com.example.zf_android.trade;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.zf_android.R;
+
+/**
+ * Created by Leo on 2015/2/26.
+ */
+public class AfterSaleGridActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_after_sale_grid);
+	}
+}
---
+++ src/com/example/zf_android/trade/AfterSaleListActivity.java
@@ -0,0 +1,9 @@
+package com.example.zf_android.trade;
+
+import android.app.ListActivity;
+
+/**
+ * Created by Leo on 2015/2/26.
+ */
+public class AfterSaleListActivity extends ListActivity {
+}
