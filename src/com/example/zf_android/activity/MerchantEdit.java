58f12102a398f2b60b15ececebeef823717a0699
ljp <976207800@qq.com>
2015-03-14 09:29:20 +0800

    d

---
+++ src/com/example/zf_android/activity/MerchantEdit.java
@@ -0,0 +1,15 @@
+package com.example.zf_android.activity;
+
+import android.os.Bundle;
+
+import com.example.zf_android.BaseActivity;
+import com.example.zf_android.R;
+
+public class MerchantEdit extends BaseActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.merchant_info);
+	}
+}
