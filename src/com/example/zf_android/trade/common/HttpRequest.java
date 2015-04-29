6dffb4d47517dbc87875382f727f98fe1a09ddf5
quqiang <qujunxi@126.com>
2015-04-29 13:47:35 +0800

    视频

--- src/com/example/zf_android/trade/common/HttpRequest.java
+++ src/com/example/zf_android/trade/common/HttpRequest.java
@@ -82,6 +82,9 @@ public class HttpRequest {
 
 	public void get(String url) {
 		if (!NetworkUtil.isNetworkAvailable(context)) {
+			if(callback == null){
+				return;
+			}
 			callback.onFailure(context.getString(R.string.network_info));
 			return;
 		}
@@ -90,6 +93,9 @@ public class HttpRequest {
 
 	public void post(String url, RequestParams requestParams) {
 		if (!NetworkUtil.isNetworkAvailable(context)) {
+			if(callback == null){
+				return;
+			}
 			callback.onFailure(context.getString(R.string.network_info));
 			return;
 		}
@@ -98,6 +104,9 @@ public class HttpRequest {
 
 	public void post(String url, HttpEntity entity) {
 		if (!NetworkUtil.isNetworkAvailable(context)) {
+			if(callback == null){
+				return;
+			}
 			callback.onFailure(context.getString(R.string.network_info));
 			return;
 		}
@@ -111,6 +120,9 @@ public class HttpRequest {
 			entity = new StringEntity(jsonParams.toString(), "UTF-8");
 			entity.setContentType("application/json;charset=UTF-8");
 		} catch (UnsupportedEncodingException e) {
+			if(callback == null){
+				return;
+			}
 			callback.onFailure(context.getString(R.string.load_data_failed));
 			return;
 		}
